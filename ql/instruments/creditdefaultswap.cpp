0ce44ed129236e5069af262f5a72d272047aed49
Guillaume Horel <guillaume.horel@gmail.com>
2018-02-26 19:56:33 -0500

    fix possible bug

--- ql/instruments/creditdefaultswap.cpp
+++ ql/instruments/creditdefaultswap.cpp
@@ -63,7 +63,7 @@ namespace QuantLib {
             .withLastPeriodDayCounter(lastPeriodDayCounter);
 
         Date effectiveUpfrontDate = schedule.calendar().advance(
-            protectionStart_, 2, Days, convention);
+            protectionStart_ - 1, 3, Days, convention);
         // '2' is used above since the protection start is assumed to be
         //   on trade_date + 1
 
